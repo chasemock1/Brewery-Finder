b04418922f2d2a7c020f9b324bac57372843d8dd
Chase Mock <chasemock1@hotmail.com>
2020-09-25 09:47:21 -0400

    Complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Api being used
 const domain = 'https://api.openbrewerydb.org/breweries'
 
 
@@ -9,27 +10,29 @@ const inputCity = document.querySelector(`#city-search`)//city input box
 const inputState = document.querySelector(`#state-search`)//state input box
 const cityStateSubmit = document.querySelector(`#submit`)//city & state submit button
 
+
+//Submit event for keywords and/or names of brewery
 submit.addEventListener('click', async ()=>{
     
     let userInput = input.value
     const response = await axios.get(`${domain}/search?query=${userInput}&per_page=25`)
-    console.log(response)
     clearList()
     renderList(response.data)
 })
 
+//Submit event for the city and state
 cityStateSubmit.addEventListener('click', async ()=>{
 
     let city = inputCity.value
     let state = inputState.value
     const response = await axios.get(`${domain}?by_state=${state}&by_city=${city}&per_page=25`)
-    console.log(response)
     clearList()
     renderList(response.data)
 })
 
 const displayBrew = document.querySelector(".brewery-list")
 
+//Function to render the list of breweries and their data
 const renderList = breweries =>{
     console.log(breweries)
     breweries.forEach(brewery => {
@@ -40,49 +43,42 @@ const renderList = breweries =>{
         const brewName = document.createElement('h3')
         brewName.innerHTML = `Name: ${brewery.name}`
         brewList.appendChild(brewName)
-        console.log(brewName)
 
         //gets and appends the State where the brewery is located
         const brewState = document.createElement('p')
         brewState.innerHTML = `State: ${brewery.state}`
         brewList.appendChild(brewState)
-        console.log(brewState)
 
         //get and appends the city where the brewery is located
         const brewCity  = document.createElement('p')
         brewCity.innerHTML = `City: ${brewery.city}`
         brewList.appendChild(brewCity)
-        console.log(brewCity)
     
         //get and appends the street where the brewery is located
         const brewStreet  = document.createElement('p')
         brewStreet.innerHTML = `Street: ${brewery.street}`
         brewList.appendChild(brewStreet)
-        console.log(brewStreet)
 
         //get and appends the phone number of the brewery 
         const brewPhone  = document.createElement('p')
         brewPhone.innerHTML = `Phone Number: ${brewery.phone}`
         brewList.appendChild(brewPhone)
-        console.log(brewPhone)
 
 
         //get and appends the Website of the brewery 
         const brewWeb  = document.createElement('p')
         brewWeb.innerHTML = `WebSite: ${brewery.website_url}`
         brewList.appendChild(brewWeb)
-        console.log(brewWeb)
-        console.log(brewList)
 
 
-
+        //All the data grab is pushed to the html
         displayBrew.append(brewList)
     });
 
 }
 
+//Clears the list
 function clearList() {
-    // write code here
     const clearElement = document.querySelector('.brewery-list')
     while(clearElement.lastChild){
       clearElement.removeChild(clearElement.lastChild)
